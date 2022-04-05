3770f1730e08809caa223c337ed9d1b0667deae1
Bjenhamin4Alfredolvchenki <66851510+Bjenhamin4Alfredolvchenki@users.noreply.github.com>
2022-04-05 22:23:44 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,6 @@ import { mdsvex } from 'mdsvex'
 import mdsvexConfig from './mdsvex.config.js'
 import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-static'
-import cloudflare from '@sveltejs/adapter-cloudflare';
 
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -21,8 +20,7 @@ const config = {
 
   kit: {
     target: '#svelte',
-    adapter: cloudflare(),
-    adapter: adapter({
+      adapter: adapter({
       pages: 'public',
       assets: 'public'
     }),
